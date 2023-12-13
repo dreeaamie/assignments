ef0cfe7b95b589bd28b07ce43be1dba6064a7f9c
dreeaamie <121973953+dreeaamie@users.noreply.github.com>
2023-12-13 19:30:30 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,20 @@
 */
 
 function isAnagram(str1, str2) {
+    if (str1.length!==str2.length){ 
+    return false;}
 
+
+const temp1=str1.split('').sort();
+const temp2=str2.split('').sort();
+
+    for(let i=0;i<temp1.length;i++)
+  {
+      if(temp1[i]!==temp2[i]){
+          return false
+      }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
