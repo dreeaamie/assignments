0f7dc9b44657bdd62f87df57b43eb56735b1cfc7
dreeaamie <121973953+dreeaamie@users.noreply.github.com>
2024-01-29 19:13:56 +0530

    Update calculator.js
    calculator

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,29 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+let string="";
+let buttons=document.querySelectorAll('.button');
+let input=document.querySelector('input');
+
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML== "="){
+        string=eval(string);
+        input.value=string;
+        }
+        else if(e.target.innerHTML== "ac"){
+string="";
+input.value=string;
+        }
+        else if(e.target.innerHTML== "del"){
+string=string.substring(0,string.length-1);
+input.value=string;       
+}
+        else {
+            string=string+e.target.innerHTML;
+            input.value=string;
+        }
+    })
+})
 
 module.exports = Calculator;
