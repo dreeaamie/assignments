5f34f7ee94c55d77513b31055e2bb497d3db346d
dreeaamie <121973953+dreeaamie@users.noreply.github.com>
2025-02-16 20:59:08 +0000

    weeek3

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -3,16 +3,35 @@ const router = Router();
 const userMiddleware = require("../middleware/user");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    let username=req.body.username
+    let password=req.body.password
+
+    await Admin.create({
+        username: username,
+        password: password
+    })
+
+    res.json({
+        message: 'Admin created successfully'
+    })
 });
 
-router.get('/courses', (req, res) => {
+router.get('/courses', async (req, res) => {
     // Implement listing all courses logic
+    let respone=await courses.find({})
+    res.json({
+        courses:response
+    })
 });
 
 router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+    // Description: Purchases a course. courseId in the URL path should be replaced with the ID of the course to be purchased.
+    // Input: Headers: { 'username': 'username', 'password': 'password' }
+    // Output: { message: 'Course purchased successfully' }
+    
+
 });
 
 router.get('/purchasedCourses', userMiddleware, (req, res) => {
